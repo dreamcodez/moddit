cf77b55459d30cd9d70efb2dce4db3e57c63b03a
vagrant <vagrant@precise64.(none)>
2012-10-19 17:40:19 +0000

    ignore epipes

--- worker.js
+++ worker.js
@@ -3,5 +3,19 @@
 //var stdin = process.openStdin();
 //stdin.on('data', function(chunk) { console.log("Got chunk", chunk); })
 
+function shutdown_on_epipe(err) {
+  if(err) {
+    if(err.errno === 'EPIPE') {
+      process.exit(0);
+    }
+    else {
+      throw err;
+    }
+  }
+}
+
+process.stdout.on('error', shutdown_on_epipe);
+process.stdin.on('error', shutdown_on_epipe);
+
 setInterval(function(){ process.stdout.write('3:foo'); }, 2000);
 
