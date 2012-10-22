2d6e618320ea5dcbd76b1748c52c75e5aa3c7025
vagrant <vagrant@precise64.(none)>
2012-10-22 16:12:47 +0000

    oops forgot something

---
+++ src/myapp_yaws_handler.erl
@@ -0,0 +1,18 @@
+-module(myapp_yaws_handler).
+
+-include("/usr/local/lib/yaws/include/yaws_api.hrl").
+-compile(export_all).
+
+box(Str) ->
+    {'div',[{class,"box"}],
+     {pre,[],Str}}.
+
+out(A) ->
+    {ehtml,
+     [{p,[],
+       box(io_lib:format("A#arg.appmoddata = ~p~n"
+                         "A#arg.appmod_prepath = ~p~n"
+                         "A#arg.querydata = ~p~n",
+                         [A#arg.appmoddata,
+                          A#arg.appmod_prepath,
+                          A#arg.querydata]))}]}.
