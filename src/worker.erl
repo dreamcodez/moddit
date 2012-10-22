5ed81e16c087f928daf138b1647d287aae1a7343
vagrant <vagrant@precise64.(none)>
2012-10-22 21:22:20 +0000

    sending commands works \!

--- src/worker.erl
+++ src/worker.erl
@@ -14,27 +14,29 @@ run (Cmd, Timeout) ->
   Port = erlang:open_port({spawn_executable, Cmd}, [exit_status]),
   [_,_,_,_,_,_,{os_pid,Pid}] = erlang:port_info(Port),
   erlang:display("worker starter as pid #" ++ integer_to_list(Pid)),
-  port_command(Port, "56:2 {\"command\": \"stylus\", \"input\": \"#foo { color: blue }\"}"),
-  loop(Port, "", Timeout).
+  true = port_command(Port, "56:2 {\"command\": \"stylus\", \"input\": \"#foo { color: blue }\"}"),
+  loop(Port, "", Timeout, 1).
 
-loop(Port, OldStream, Timeout) ->
+loop(Port, Stream, Timeout, JobId) when is_port(Port) ->
   receive
     {Port, {data, NewStream}} ->
-      Stream = OldStream++NewStream,
-      {Messages, AdjStream} = parse_netstrings(Stream),
+      {Messages, AdjStream} = parse_netstrings(Stream++NewStream),
       case length(Messages) > 0 of
         true ->
           erlang:display({messages, Messages}),
-          loop(Port, AdjStream, Timeout);
+          loop(Port, AdjStream, Timeout, JobId);
         false ->
-          loop(Port, AdjStream, Timeout)
+          loop(Port, AdjStream, Timeout, JobId)
       end;
     {Port, {exit_status, _}} ->
       throw(worker_exit);
+    {Pid, job_request, Msg} when is_pid(Pid) ->
+      JobRequest = integer_to_list(JobId) ++ " " ++ Msg,
+      NetString = integer_to_list(length(JobRequest)) ++ ":" ++ JobRequest,
+      true = port_command(Port, NetString),
+      loop(Port, Stream, Timeout, JobId + 1);
     shutdown ->
-      throw(shutdown);
-    Other ->
-      erlang:display(Other)
+      throw(shutdown)
   after Timeout ->
     port_close(Port),
     throw(timeout)
