0c37ecf45c308510dafe3115cf572371d4e45401
vagrant <vagrant@precise64.(none)>
2012-10-23 11:17:19 +0000

    whee i can render a css doc from node land in yaws

--- src/myapp_yaws_handler.erl
+++ src/myapp_yaws_handler.erl
@@ -3,9 +3,15 @@
 -include("/usr/local/lib/yaws/include/yaws_api.hrl").
 -compile(export_all).
 
+bin_to_hexstr(Bin) ->
+  lists:flatten([io_lib:format("~2.16.0b", [X]) ||
+    X <- binary_to_list(Bin)]).
+
 out(A) ->
   JobSpec = {{command, stylus}, {input, <<".foo\n  color blue">>}},
   JobRequest = lists:flatten(jsonerl:encode(JobSpec)),
   {ok, CSS} = worker:do_job({worker, 'worker@precise64'}, JobRequest),
-  {content, "text/css", CSS}.
+  ETag = bin_to_hexstr(crypto:hash(sha, CSS)),
+  RespHeaders = [{header, "ETag: " ++ ETag}],
+  [{allheaders, RespHeaders}, {content, "text/css", CSS}].
 
