0220cc77e95ef12a0b46277a4da4653d321fa122
vagrant <vagrant@precise64.(none)>
2012-10-23 00:01:28 +0000

    latest stuff

--- src/loadtest.erl
+++ src/loadtest.erl
@@ -2,7 +2,7 @@
 -export([start/0]).
 
 start() ->
-  loop(1000).
+  loop(100).
   %loop(1).
 
 loop(Times) when Times >= 0 ->
--- src/worker.erl
+++ src/worker.erl
@@ -1,5 +1,5 @@
 -module(worker).
--export([start_link/0]).
+-export([start_link/0, do_job/1]).
 
 start_link() ->
   Pid = spawn_link(fun run/0),
@@ -10,12 +10,27 @@ run() ->
   %run("./worker.pl", 5000).
   run("./worker.js", 16000).
 
-run (Cmd, Timeout) ->
+run(Cmd, Timeout) ->
   Port = erlang:open_port({spawn_executable, Cmd}, [exit_status]),
   [_,_,_,_,_,_,{os_pid,Pid}] = erlang:port_info(Port),
   erlang:display("worker starter as pid #" ++ integer_to_list(Pid)),
   true = port_command(Port, "56:2 {\"command\": \"stylus\", \"input\": \"#foo { color: blue }\"}"),
   loop(Port, "", Timeout, 1).
+
+parse_response_frame(Frame) ->
+  case Frame of
+    "hb" ->
+      heartbeat;
+    Frame ->
+      Raw = string:tokens(Frame, " "),
+      JobId = list_to_integer(lists:nth(1, Raw)),
+      ErrCode = list_to_integer(lists:nth(2, Raw)),
+      Msg = string:join(lists:nthtail(2, Raw), " "),
+      {JobId, ErrCode, Msg}
+  end.
+
+do_job(JobRequest) ->
+  stub.
 
 loop(Port, Stream, Timeout, JobId) when is_port(Port) ->
   receive
@@ -23,7 +38,13 @@ loop(Port, Stream, Timeout, JobId) when is_port(Port) ->
       {Messages, AdjStream} = parse_netstrings(Stream++NewStream),
       case length(Messages) > 0 of
         true ->
-          erlang:display({messages, Messages}),
+          % process table for jobid -> pid mapping
+          HandleMessage =
+            fun(Msg) ->
+              JobRes = parse_response_frame(Msg),
+              erlang:display(JobRes)
+            end,
+          lists:map(HandleMessage, Messages),
           loop(Port, AdjStream, Timeout, JobId);
         false ->
           loop(Port, AdjStream, Timeout, JobId)
@@ -31,8 +52,8 @@ loop(Port, Stream, Timeout, JobId) when is_port(Port) ->
     {Port, {exit_status, _}} ->
       throw(worker_exit);
     {Pid, job_request, Msg} when is_pid(Pid) ->
-      JobRequest = integer_to_list(JobId) ++ " " ++ Msg,
-      NetString = integer_to_list(length(JobRequest)) ++ ":" ++ JobRequest,
+      Frame = integer_to_list(JobId) ++ " " ++ Msg,
+      NetString = integer_to_list(length(Frame)) ++ ":" ++ Frame,
       true = port_command(Port, NetString),
       loop(Port, Stream, Timeout, JobId + 1);
     shutdown ->
@@ -52,7 +73,7 @@ rparse_netstrings(LenStr, Stream, Strings) ->
       [Head | Tail] = Stream,
       case [Head] == ":" of
         true ->
-          {Len, []} = string:to_integer(LenStr),
+          Len = list_to_integer(LenStr),
           TailLen = string:len(Tail),
           case Len =< TailLen of
             true ->
