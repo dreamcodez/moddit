d79ad1f9c29f2407a9d17a2c61135d1d1d4d0067
mae <mae@crapple.local>
2012-10-25 14:01:03 -0700

    aggregate perf stats

--- src/loadtest.erl
+++ src/loadtest.erl
@@ -2,28 +2,40 @@
 -export([start/0]).
 
 start() ->
-  spawn(fun() -> loop(100) end),
-  spawn(fun() -> loop(100) end),
-  spawn(fun() -> loop(100) end),
-  spawn(fun() -> loop(100) end),
-  loop(1000).
-  %loop(1).
+  spawn(fun() -> loop(100, []) end),
+  spawn(fun() -> loop(100, []) end),
+  spawn(fun() -> loop(100, []) end),
+  spawn(fun() -> loop(100, []) end),
+  loop(1000, []).
 
-loop(Times) when Times >= 0 ->
+now_micro() ->
+  {Mega,Secs,Micro} = now(),
+  (Mega * 1000000 + Secs) * 1000000 + Micro.
+
+average(List) when is_list(List) ->
+  lists:sum(List) / length(List).
+
+loop(Times, Durations) when Times >= 0 ->
   case Times > 0 of
     true ->
+      Before = now_micro(),
       {ok, Output} = worker:do_job(
         {worker, 'worker@precise64'},
         "{\"command\": \"stylus\", \"input\": \".foo\\n  color blue\"}"),
-      erlang:display(Output),
-      loop(Times - 1);
+      After = now_micro(),
+      DurationMs = (After - Before) / 1000,
+      erlang:display({output, Output, jobtime_ms, DurationMs}),
+      loop(Times - 1, Durations ++ [DurationMs]);
     false ->
       receive
         Other ->
           erlang:display(Other),
-          loop(0)
+          loop(0, Durations)
       after 10000 ->
-        done
+        [Min] = io_lib:format("~.2f", [lists:min(Durations)]),
+        [Max] = io_lib:format("~.2f", [lists:max(Durations)]),
+        [Avg] = io_lib:format("~.2f", [average(Durations)]),
+        erlang:display({jobtime_ms_totals, {min, Min, max, Max, avg, Avg}})
       end
   end.
 
